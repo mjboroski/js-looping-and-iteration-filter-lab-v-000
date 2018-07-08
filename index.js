292222f5c9ec1e7c7b54db57e5943dc941dbe304
Michael Boroski <mjboroski@gmail.com>
2018-07-08 05:18:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function findMatching(){
-
+function findMatching(list, name){
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
 }
 
-function fuzzyMatch(){
 
+function fuzzyMatch(){
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
 
 function matchName(){
-
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
 }
